e109f3b7a11f239a8256518cb8d999266f9cdd15
Eliezer Garcia <jueliezermdp@gmail.com>
2022-11-30 08:19:40 -0300

    correção na ordenação de ranking
    correção na ordenação de ranking

--- src/main/java/com/gamecms/olddestiny/Services/OnlyRead.java
+++ src/main/java/com/gamecms/olddestiny/Services/OnlyRead.java
@@ -11,13 +11,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.*;
 
 @Service
 public class OnlyRead {
     File conta;
-    
+
 
     public static String getInitialChar(String accountName){
         return isEtcAccount(accountName) ? "ETC" : accountName.substring(0,1);
@@ -30,7 +31,7 @@ public class OnlyRead {
     }
 
     public static String getRankingPlayer(Path path) throws IOException {
-                List<PlayerRanking> rankings = new ArrayList<>();
+                PlayerRanking[] rankings = {};
                 List<String> linhas = Files.readAllLines(path);
 
                 linhas.forEach(
@@ -41,7 +42,7 @@ public class OnlyRead {
                             player.nome = linhaPlayer[1];
                             player.level = linhaPlayer[2];
                             player.classe = linhaPlayer[3];
-                            rankings.add(player);
+                            rankings[ (int) Arrays.stream(rankings).count() ] = player;
                         }
                 );
 
