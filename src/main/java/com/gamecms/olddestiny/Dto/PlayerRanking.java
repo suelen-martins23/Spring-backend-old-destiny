91f5161c89317f09a7cdfa57be233e7120f61ede
Eliezer Garcia <jueliezermdp@gmail.com>
2022-11-30 08:16:06 -0300

    métodos de ranking
    métodos de ranking

---
+++ src/main/java/com/gamecms/olddestiny/Dto/PlayerRanking.java
@@ -0,0 +1,8 @@
+package com.gamecms.olddestiny.Dto;
+
+public class PlayerRanking {
+    public String posicao;
+    public String nome;
+    public String classe;
+    public String level;
+}
--- src/main/java/com/gamecms/olddestiny/Services/OnlyRead.java
+++ src/main/java/com/gamecms/olddestiny/Services/OnlyRead.java
@@ -1,17 +1,23 @@
 package com.gamecms.olddestiny.Services;
 
+import com.gamecms.olddestiny.Dto.PlayerRanking;
+import com.google.gson.Gson;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.*;
 
 @Service
 public class OnlyRead {
     File conta;
-
-
+    
 
     public static String getInitialChar(String accountName){
         return isEtcAccount(accountName) ? "ETC" : accountName.substring(0,1);
@@ -20,8 +26,26 @@ public class OnlyRead {
     public static boolean isEtcAccount(String accountName){
         Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
         Matcher m = p.matcher(accountName);
-
         return m.find();
+    }
+
+    public static String getRankingPlayer(Path path) throws IOException {
+                List<PlayerRanking> rankings = new ArrayList<>();
+                List<String> linhas = Files.readAllLines(path);
+
+                linhas.forEach(
+                        n->{
+                            String[] linhaPlayer = StringUtils.split(n, " ");
+                            PlayerRanking player = new PlayerRanking();
+                            player.posicao = linhaPlayer[0];
+                            player.nome = linhaPlayer[1];
+                            player.level = linhaPlayer[2];
+                            player.classe = linhaPlayer[3];
+                            rankings.add(player);
+                        }
+                );
+
+                return new Gson().toJson(rankings);
     }
 
 }
